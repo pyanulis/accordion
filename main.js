bd47b17d725ce1bcd0307e34b095e56933dc98d3
pyanulis <pyanulis@gmail.com>
2016-03-03 03:56:33 +0300

    accordion

---
+++ main.js
@@ -0,0 +1,17 @@
+var item = document.getElementsByClassName('accordionItem');
+var header = document.getElementsByClassName('accordionItemHeading');
+
+for (i = 0; i < header.length; i++) {
+    header[i].addEventListener('click', toggleItem, false);
+}
+
+function toggleItem() {
+    var itemClass = this.parentNode.className;
+    for (i = 0; i < item.length; i++) {
+        item[i].className = 'accordionItem close';
+    }
+    
+    if (itemClass == 'accordionItem close') {
+        this.parentNode.className = 'accordionItem open';
+    }
+}
